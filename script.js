0a219ace767c10a1de30b2e0eb4f61904ad74358
Lundet <hannes.j.lundin@gmail.com>
2023-02-06 11:28:30 +0100

    Fixed a fetch to get mars rover pictures to the index.html

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const button = document.querySelector("button");
 
-const container = document.querySelector("#container")
+const apiKey = "api_key=5w0oALe8aKV1rx64FoAQKn6vCeDAjaeTvGangKyT"
 
-const apiKey = "https://api.nasa.gov/planetary/apod?api_key=5w0oALe8aKV1rx64FoAQKn6vCeDAjaeTvGangKyT"
+const searchInput = document.getElementById("searchInput")
 
 
 
 button.addEventListener("click", () => {
-    fetch("https://api.nasa.gov/planetary/apod?api_key=5w0oALe8aKV1rx64FoAQKn6vCeDAjaeTvGangKyT").then(Response => Response.json()).then((data) => {
-       
-        console.log("data", data)  
-           
-        
-    })
+    fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=5w0oALe8aKV1rx64FoAQKn6vCeDAjaeTvGangKyT").then(Response => Response.json()).then((data) => {
+
+    console.log(data);
+    const photos = data.photos;
+    
+    let output = '';
+    photos.forEach(photo => {
+      output += `<li><img src="${photo.img_src}" alt="${photo.camera.full_name}"/></li>`;
+    });
+    document.getElementById("images").innerHTML = output;
+  })
+  
 });
